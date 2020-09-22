178c7018b1ea40f4ab2a553dd0dc17b6914cf522
etolley <eetolley@gmail.com>
2020-09-22 10:38:29 +0200

    bit of cleanup

---
+++ install.sh
@@ -0,0 +1,4 @@
+virtualenv --system-site-packages venvs/astropy-venv
+source venvs/astropy-venv/bin/activate
+pip install astropy
+pip install matplotlib
---
+++ python/examine_fits_file.py
@@ -0,0 +1,25 @@
+from astropy.io import fits
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+import numpy as np
+from astropy.visualization import astropy_mpl_style
+plt.style.use(astropy_mpl_style)
+
+# the array data of each HDU is accessed with mmap
+# rather than being read into memory all at once.
+with fits.open("sample_cube.fits") as hdul:
+	hdul.info()
+
+	# the cards of the file
+	print(repr(hdul[0].header))
+	data = hdul[0].data
+
+	print(data.shape)
+
+	
+	plt.figure()
+	plt.imshow( np.abs(data[-1,:,:]), cmap = 'bone')
+	plt.colorbar()
+	plt.xlabel(hdul[0].header["CTYPE1"])
+	plt.ylabel(hdul[0].header["CTYPE2"])
+	plt.show()
